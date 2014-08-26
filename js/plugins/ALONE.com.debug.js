b5903a5915f2007562f984fe218bd940e6ca25dd
ystskm <brilliantpenguin@gmail.com>
2014-08-26 21:39:28 +0900

    update for new debug

--- js/plugins/ALONE.com.debug.js
+++ js/plugins/ALONE.com.debug.js
@@ -1,25 +1,34 @@
 // window debug function set (both Node and client version) >>
 var util, debug;
-if(typeof global == 'undefined') {
-  // client js
-  util = console;
-  debug = window.xdebug;
-  window.xdebug = debug_module;
-} else {
-  // node js
-  util = require('util');
-  debug = process.env.NODE_DEBUG;
-  module.exports = debug_module;
+
+// window
+if(typeof window != 'undefined' && typeof console != 'undefined') {
+  try {
+    util = console;
+    debug = window.env && window.env.DEBUG;
+    window.xdebug = debug_module;
+  } catch(e) {
+  }
 }
+
+// node.js module
+if(typeof module != 'undefined' && typeof process != 'undefined') {
+  try {
+    util = require('util');
+    debug = process.env.NODE_DEBUG;
+    module.exports = debug_module;
+  } catch(e) {
+  }
+}
+
 function debug_module(category, filename) {
-  if(!filename)
-    filename = category;
-  if(debug
-    && (debug == 'true' || (new RegExp(category + '|' + filename).test(debug)))) {
-    return function(s) {
-      util.error(filename + '.js : ' + (typeof s == 'function' ? s(): s));
-    };
-  } else
-    return function() {
-    };
+  !filename && (filename = category);
+  try {
+    if(debug == 'true' || (new RegExp(category + '|' + filename).test(debug)))
+      return function(s) {
+        util.error(filename + '.js : ' + (typeof s == 'function' ? s(): s));
+      };
+  } catch(e) {
+  }
+  return Function()
 };
